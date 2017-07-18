8f96b994ad30129717d43c2de598d2add273aff8
Adam Gray <adamkruschkagray@gmail.com>
2017-07-18 10:06:13 -0400

    shoulda matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,29 @@ RSpec.describe User, type: :model do
   it { should_not have_valid(:username).when(nil) }
 
   it { should have_valid(:email).when('user@example.com') }
-  it { should_not have_valid(:username).when(nil, 'user.com', 'user') }
+  it { should_not have_valid(:email).when(nil, 'user.com', 'user') }
+
+  it "has matching passwword confirmation" do
+    user_one = User.new
+    user_one.email = "example@example.com"
+    user_one.admin = false
+    user_one.username = 'example'
+
+    user_one.password = "password"
+    user_one.password_confirmation = 'anther password'
+
+    expect(user_one).to_not be_valid
+    expect(user_one.errors[:password_confirmation]).to_not be_blank
+
+    user_two = User.new
+    user_two.email = "example@example.com"
+    user_two.admin = true
+    user_two.username = 'example'
+    user_two.password='password'
+    user_two.password_confirmation='password'
+    expect(user_two).to be_valid
+  end
+
+
 
 end
