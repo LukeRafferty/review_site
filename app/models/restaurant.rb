8444092dba0a1018dbd3a8dcd92a2b1d9384c84c
andy2012v <andy2012v@gmail.com>
2017-07-19 13:04:55 -0400

    Continuing changes and updates to forms

---
+++ app/models/restaurant.rb
@@ -0,0 +1,14 @@
+class Restaurant < ApplicationRecord
+  
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip_code, presence: true
+  validates :description, presence: true
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
---
+++ app/models/review.rb
@@ -0,0 +1,9 @@
+class Review < ApplicationRecord
+  
+  validates :description, presence: true
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
