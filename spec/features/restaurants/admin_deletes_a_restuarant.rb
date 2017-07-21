d676dcdca185fd5f56d355c1456df0a8758a76a5
Luke Rafferty <luke.mingins.rafferty@gmail.com>
2017-07-21 10:19:44 -0400

    added sad path test, fixed scenario error

--- spec/features/restaurants/admin_deletes_a_restuarant.rb
+++ spec/features/restaurants/admin_deletes_a_restuarant.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 feature "admin edits restaurant" do
 
-  scenario "goes to the edit page via show page" do
+  scenario "admins can delete restaurants" do
 
     admin = FactoryGirl.create(:user, admin: true)
     visit root_path
@@ -27,7 +27,25 @@ feature "admin edits restaurant" do
     expect(page).to have_content("Restaurant was successfully deleted")
 
   end
+  scenario "users cannot delete restaurants" do
 
+    admin = FactoryGirl.create(:user, admin: false)
+    visit root_path
+    click_on 'Sign In'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+    visit new_restaurant_path
 
-
+    fill_in 'Name', with: "Burger King"
+    fill_in 'Address', with: "78 Summer st"
+    fill_in 'City', with: "Boston"
+    fill_in 'State', with: "MA"
+    fill_in 'Zip Code', with: "02111"
+    fill_in 'Description', with: "Have it your way!"
+    expect(page).to have_button 'Add Restaurant'
+    click_on "Add Restaurant"
+    expect(page).to have_content "Burger King"
+    expect(page).to_not have_content "destroy"
+  end
 end
