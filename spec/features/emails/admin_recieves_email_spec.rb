a822ac45f1573edbc1adb9e9944955ab3b8c6bab
Luke Rafferty <luke.mingins.rafferty@gmail.com>
2017-07-25 12:58:20 -0400

    sad path test for email

--- spec/features/emails/admin_recieves_email_spec.rb
+++ spec/features/emails/admin_recieves_email_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 feature "website sends email to admin when reivew is posted on a restaurant" do
-  scenario "review a product" do
+  scenario "someone reviews a restaurant" do
 
   user = FactoryGirl.create(:user, admin: false)
   restaurant = FactoryGirl.create(:restaurant, user_id: user.id)
@@ -23,6 +23,30 @@ feature "website sends email to admin when reivew is posted on a restaurant" do
   expect(page).to have_content('Review Added Successfully')
 
   expect(ActionMailer::Base.deliveries.count).to eq(1)
+  end
+  scenario "incomplete review form sends no email" do
+    user = FactoryGirl.create(:user, admin: false)
+    restaurant = FactoryGirl.create(:restaurant, user_id: user.id)
+
+
+    visit root_path
+    click_on "Sign In"
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+
+    visit restaurant_path(restaurant.id)
+
+    click_button 'Add Review'
+    expect(page).to have_content(restaurant.name)
+    fill_in 'Body', with: 'Total garbage'
+    click_button 'Add Review'
+    expect(page).to have_content('error!')
+    expect(page).to have_content('can\'t be blank')
+    expect(page).to have_content("New Review Form for: #{restaurant.name}")
+
+    expect(ActionMailer::Base.deliveries.count).to eq(0)
+
   end
 
 
