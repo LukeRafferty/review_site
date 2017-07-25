4b61556aba25073a3492ada4a94e8974e6de0e7c
andy2012v <andy2012v@gmail.com>
2017-07-25 17:25:16 -0400

    Fixed styling for foundation in index page of restaurants

--- react/src/Components/RestaurantIndexTile.js
+++ react/src/Components/RestaurantIndexTile.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const RestaurantIndexTile = props => {
   return(
-    <div className={'row small-8 columns small-offset-2 index-container end'}>
+    <div className={'row small-8 columns small-centered index-container'}>
       <div className={'row'}>
         <div className={'small-9 columns index-link-and-info-container'}>
           <h3 className={'index-link'}><a href={`/restaurants/${props.id}`}>{props.name}</a></h3>
