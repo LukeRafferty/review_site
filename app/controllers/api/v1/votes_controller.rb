853b2df521446aea4ae890006173ae464d4b8f31
Adam Gray <adamkruschkagray@gmail.com>
2017-07-26 09:51:21 -0400

    Strengthen backend for votes

--- app/controllers/api/v1/votes_controller.rb
+++ app/controllers/api/v1/votes_controller.rb
@@ -1,9 +1,24 @@
 class Api::V1::VotesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  #this is an unconventional method - it creates AND updates
   def create
-    vote = Vote.create(review_id: params[:review_id], user_id: current_user.id, up_down: params[:up_down])
-    render json: vote, adapter: :json
+    #check if the vote already exists
+    existing_vote = Vote.find_by(user_id: current_user.id, review_id: params[:review_id])
+
+    if existing_vote
+      #if it does, update the vote's up_down value
+      existing_vote.update_attributes(up_down: params[:up_down])
+      render json: existing_vote, adapter: :json
+    else
+      #otherwise, create a new vote
+      vote = Vote.new(review_id: params[:review_id], user_id: current_user.id, up_down: params[:up_down])
+      if vote.save
+        render json: vote, adapter: :json
+      else
+        render json: vote.errors, adapter: :json
+      end
+    end
   end
 
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,4 @@
 class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :review
-
-  validates :up_down, presence: true
 end
