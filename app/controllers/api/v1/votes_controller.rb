617e14902e71f6475b170e1765ad1e7b83892dd8
Adam Gray <adamkruschkagray@gmail.com>
2017-07-26 10:59:15 -0400

    Refactor voting system

--- app/controllers/api/v1/votes_controller.rb
+++ app/controllers/api/v1/votes_controller.rb
@@ -6,23 +6,31 @@ class Api::V1::VotesController < ApplicationController
 
     #to make testing and development work
     data = nil
-    if Rails.env.development? || Rails.env.production?
-      data = JSON.parse(request.body.read)
-    else
+    if Rails.env.test?
       data = params
+    else
+      data = JSON.parse(request.body.read)
     end
 
 
     #check if the vote already exists
-    existing_vote = Vote.find_by(user_id: current_user.id, review_id: data[:review_id])
+    existing_vote = Vote.find_by(
+      user_id: current_user.id,
+      review_id: data["review_id"]
+    )
 
     if existing_vote
       #if it does, update the vote's up_down value
-      existing_vote.update_attributes(up_down: data[:up_down])
+      existing_vote.update_attributes(up_down: data["up_down"])
       render json: existing_vote, adapter: :json
     else
       #otherwise, create a new vote
-      vote = Vote.new(review_id: data[:review_id], user_id: current_user.id, up_down: data[:up_down])
+      vote = Vote.new(
+        review_id: data["review_id"],
+        user_id: current_user.id,
+        up_down: data["up_down"]
+      )
+      binding.pry
       if vote.save
         render json: vote, adapter: :json
       else
