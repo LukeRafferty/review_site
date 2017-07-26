06eab99754ae0d8eaf34e3e6b3d2fcdb5a910481
Luke Rafferty <luke.mingins.rafferty@gmail.com>
2017-07-26 11:38:53 -0400

    initial commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    if @user.destroy
+      redirect_to root_url, notice: "User deleted."
+    end
+  end
 end
 # user_signed_in?
 # current_user
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :reviews, except: [:index, :show]
   end
 
+  resources :users, only: [:show, :destroy, :index]
+
   namespace :api do
     namespace :v1 do
       resources :restaurants, only: [:index, :show]
