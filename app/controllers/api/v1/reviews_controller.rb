e637759a0b9cb0b6cdd3d57ab6b9b8d3e34af85a
Adam Gray <adamkruschkagray@gmail.com>
2017-07-27 10:40:31 -0400

    Add tests

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -2,8 +2,23 @@ class Api::V1::ReviewsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def destroy
-    
+    if Rails.env.test?
+      data = params
+    else
+      data = JSON.parse(request.body.read)
+    end
 
+    review = Review.find(data["id"])
+
+    if current_user.id == review["user_id"] || current_user.admin
+      if Review.destroy(data["id"])
+        render json: { result: "Review deleted successfully" }, adapter: :json
+      else
+        render json: { result: "Failed to delete review" }, adapter: :json
+      end
+    else
+      render json: { result: "You do not have permission to delete this review" }, adapter: :json
+    end
   end
 
 end
