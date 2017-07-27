7c8553a1225e6ddc379708226892f01cfc84b056
Colin Burgess <burgess.colin3@gmail.com>
2017-07-27 16:35:53 -0400

    Restaurant index styling

--- react/src/Components/RestaurantIndexTile.js
+++ react/src/Components/RestaurantIndexTile.js
@@ -3,7 +3,6 @@ import React from 'react'
 const RestaurantIndexTile = props => {
   return(
     <div className={'row small-8 columns small-centered index-container'}>
-      <div className={'row'}>
         <div className={'index-link-and-info-container'}>
           <h3 className={'restaurant_link'}><a href={`/restaurants/${props.id}`}>{props.name}</a></h3>
           <div className={'index-info'}>
@@ -11,7 +10,6 @@ const RestaurantIndexTile = props => {
             <p>Description: {props.description}</p>
           </div>
         </div>
-      </div>
     </div>
   )
 }
